d2baec89cc13d980fb4d2b3c835065e32c98ea7f
Asif Shagor <38321246+riddle989@users.noreply.github.com>
2023-07-20 13:01:33 +0600

    Update VxLan-docker.sh

--- VxLan-docker.sh
+++ VxLan-docker.sh
@@ -23,9 +23,9 @@ apt-get install tcpdump
 
 
 echo -e "\n ############ Creating Subnet for Docker ############ \n"
-read -p "Enter the docker Subnet CIDR(Formant Hint: 10.1.1.20/24)..." SUBNET_CIDR
+read -p "Enter the docker Subnet CIDR(Formant Hint: 10.1.1.20/24) ===> " SUBNET_CIDR
 echo -e "\n"
-read -p "Enter the docker Subnet Name..." SUBNET_NAME
+read -p "Enter the docker Subnet Name ===> " SUBNET_NAME
 echo -e "\n"
 SUBNET_ID=$(docker network create --subnet "${SUBNET_CIDR}" "${SUBNET_NAME}")
 if [[ "${?}" -ne 0 ]]
@@ -40,9 +40,10 @@ echo -e "\n"
 
 
 echo -e "\n ############ Creating Docker Container ############ \n"
-read -p "Enter the docker Container IP Address..." CONTAINER_IP
+NETWORK_RANGE=$(sipcalc "${SUBNET_CIDR}" | grep "Network range")
+read -p "Enter the docker Container IP Address.${NETWORK_RANGE} ===> " CONTAINER_IP
 echo -e "\n"
-read -p "Enter the docker Container Name..." CONTAINER_NAME
+read -p "Enter the docker Container Name ===> " CONTAINER_NAME
 echo -e "\n"
 CONTAINER_ID=$(docker run -d --name "${CONTAINER_NAME}" --net "${SUBNET_NAME}" --ip "${CONTAINER_IP}" ubuntu sleep 3000)
 if [[ "${?}" -ne 0 ]]
@@ -55,11 +56,11 @@ echo -e "Container Id ===> ${CONTAINER_ID} \n"
 
 
 echo -e "\n ############ Creating VxLan ############ \n"
-read -p "Enter the VxLan Name..." VXLAN_NAME
+read -p "Enter the VxLan Name ===> " VXLAN_NAME
 echo -e "\n"
-read -p "Enter the VxLan ID / VNI ..." VNI
+read -p "Enter the VxLan ID / VNI ===> " VNI
 echo -e "\n"
-read -p "Enter the Remote machine/ VTEP IP Address..." VTEP_IP
+read -p "Enter the Remote machine/ VTEP IP Address ===> " VTEP_IP
 VXLAN_CREATION=$(ip link add "${VXLAN_NAME}" type vxlan id "${VNI}" remote "${VTEP_IP}" dstport 4789 dev enp0s8)
 if [[ "${?}" -ne 0 ]]
 then
@@ -76,6 +77,7 @@ then
     echo -e "VxLan container creation unsuccessful. Msg: ${VXLAN_STATUS}"
     exit 1
 fi
+echo "Successfully set up the VxLan"
 
 
 echo -e "\n ############ Add VxLan to the docker bridge ############ \n"
@@ -85,7 +87,7 @@ then
     echo -e "Connection to VxLan from Docekr bridge network unsuccessful. Msg: ${BRIDGE_CONNECTION}"
     exit 1
 fi
-
+echo "Successfully attached the VxLan to the docekr bridge network"
 
 echo -e "\n ############ Get into Doker Container & Installing network tools ############ \n"
 
@@ -99,26 +101,3 @@ apt-get install net-tools
 apt-get install iputils-ping
 apt-get install tcpdump
 exit
-
-
-# 10.1.1.20/24
-# vxlan-net
-
-# 10.1.1.30
-# node_01
-
-# vxlan-demo
-# 100
-# 10.0.1.20
-
-
-
-# 10.1.1.22/24
-# vxlan-net
-
-# 10.1.1.40
-# node_02
-
-# vxlan-demo
-# 100
-# 10.0.1.10
